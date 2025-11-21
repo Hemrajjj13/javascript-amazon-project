1f4b2876297cb9cd5efe7dfeeaa9aa7a35b6d599
Hemrajjj13 <iamhemraj13@gmail.com>
2025-11-21 14:15:34 +0530

    ..

---
+++ test-jasmine/utils/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../../scripts/utils/money.js';
+
+describe('test suit: formatCurrency', () => {
+    it('converts cents to dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('round up the nearest cent', () => {
+        expect(formatCurrency(200.5)).toEqual('2.01');
+    });
+});
